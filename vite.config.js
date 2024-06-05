4db8c81aef44fdaa3300cdfec308f6d343de1c4a
cnorzen <thaibinh403h@gmail.com>
2024-06-05 17:02:43 +0700

    fix loi deploy thu hai lan nua

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [
-    reactRefresh(),
     react()
   ],
   build: {
