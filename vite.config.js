37736ebca81770348e7b25d3f27a601c9f58b3f8
cnorzen <thaibinh403h@gmail.com>
2024-06-05 16:58:54 +0700

    fix loi deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    reactRefresh(),
+    react()
+  ],
+  build: {
+    outDir: "dist"
+  }
 });
